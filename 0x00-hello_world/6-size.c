138d08b2f728cc895659fc546bee18a09106c051
Your Name <you@example.com>
2022-03-10 15:23:47 -0800

    task 6 commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,8 @@
-#!/bin/bash
 #include <stdio.h>
-/**
- *  * main - main block
- *   * Return: 0
- *    */
+/*
+ *   main - main block
+ *    Return: 0
+*/
 int main(void)
 {
 		printf("Size of a char: %i byte(s)\n", sizeof(char));
