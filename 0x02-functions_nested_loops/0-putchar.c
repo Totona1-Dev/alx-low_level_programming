e0e3bb01f66f2dadcd193aa851f8f38c5075454f
Your Name <you@example.com>
2022-03-16 04:02:41 -0700

    task 0 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
-
-
-/*
- * main entry point of the program
- *
- * Return always 0 (success)
- */
+#include "main.h"
+/**
+ * main - entry block
+ * @void: no arguments
+ * Return: 0
+ **/
 int main(void)
-{
-	printf("_putchar\n");
+{char ermiacs[] = "_putchar";
+
+	int c
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(ermias[c]);
+	}
+	_putchar('\n');
 	return (0);
 }
 
